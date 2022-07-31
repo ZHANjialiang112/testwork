690cb20ff1e09e072d32de84ececf31d0800a2e3
zjl <2655919500@qq.com>
2022-07-31 22:37:19 +0800

    多线程测试

---
+++ src/zjl/com/threadpool/ThreadPoolTest.java
@@ -0,0 +1,31 @@
+package zjl.com.threadpool;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolTest {
+    private static ThreadPoolExecutor threadPoolExecutor = null;
+
+    private ThreadPoolTest(){}
+
+    public static ThreadPoolExecutor getThreadPoolExecutor(){
+        if (threadPoolExecutor == null) {
+            synchronized (ThreadPoolTest.class){
+                if (threadPoolExecutor == null) {
+                    threadPoolExecutor = new ThreadPoolExecutor(4, 8, 50, TimeUnit.SECONDS,
+                            new LinkedBlockingDeque<>(10), new ThreadFactory() {
+                        @Override
+                        public Thread newThread(Runnable r) {
+                            return new Thread(r);
+                        }
+                    }, new RejectedExecutionHandler() {
+                        @Override
+                        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                            System.out.println("线程被拒绝执行："+r.getClass().getName());
+                        }
+                    });
+                }
+            }
+        }
+        return threadPoolExecutor;
+    }
+}
